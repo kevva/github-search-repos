37fbee04b12f5c75af78bb8cbd8c00bfb94fbdfd
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-16 22:01:20 +0700

    typo

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var githubTokenUser = require('./');
+var githubSearchRepos = require('./');
 var test = require('ava');
 
 test(function (t) {
 	t.plan(4);
 	var token = '523ef691191741c99d5afbcfe58079bfa0038771';
 
-	githubTokenUser('gulp+language:javascript', {token: token}, function (err, data) {
+	githubSearchRepos('gulp+language:javascript', {token: token}, function (err, data) {
 		t.assert(!err, err);
 		t.assert(!data.incomplete_results);
 		t.assert(data.items.length);
