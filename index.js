779b898a06e95618c5ef3be6e548d84f2d013286
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-16 17:58:42 +0100

    Fallback to `opts` if `cb` is `undefined`
    Fixes #3.

--- index.js
+++ index.js
@@ -3,16 +3,26 @@
 var got = require('got');
 
 module.exports = function (query, opts, cb) {
+	opts = opts || {};
+
 	if (typeof query !== 'string') {
 		throw new Error('Search query required');
+	}
+
+	if (!cb && typeof opts === 'function') {
+		cb = opts;
+		opts = {};
 	}
 
 	var url = 'https://api.github.com/search/repositories?q=' + query;
 	var headers = {
 		Accept: 'application/vnd.github.v3+json',
-		Authorization: 'token ' + opts.token,
 		'User-Agent': 'https://github.com/kevva/github-search-repos'
 	};
+
+	if (opts.token) {
+		headers.Authorization = 'token ' + opts.token;
+	}
 
 	if (opts.sort) {
 		url += '&sort=' + opts.sort;
